77d364272607d0196bd5ea674abbddd517832378
abhishek jaswal <abhishekjaswal1122@gmail.com>
2025-10-24 15:45:45 +0530

    updated images

--- app/components/FullScreenCarousel.tsx
+++ app/components/FullScreenCarousel.tsx
@@ -1,4 +1,3 @@
-// components/FullScreenCarousel.tsx
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,27 +8,31 @@ import "swiper/css";
 const images = [
   "/images/ladakh.jpg",
   "/images/ladakh2.jpg",
-  "/images/ladakh3.jpg"
+  "/images/ladakh3.jpg",
 ];
 
 export default function FullScreenCarousel() {
   return (
-    <Swiper
-      modules={[Autoplay]}
-      autoplay={{ delay: 3000, disableOnInteraction: false }}
-      loop={true}
-      className="w-screen h-screen"
-    >
-      {images.map((src, idx) => (
-        <SwiperSlide key={idx} className=" w-screen h-screen relative">
-          <Image
-            src={src}
-            alt={`Slide ${idx + 1}`}
-            fill
-            className="object-cover"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="w-full h-[60vh] sm:h-[70vh] md:h-[85vh] lg:h-screen">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+        className="w-full h-full"
+      >
+        {images.map((src, idx) => (
+          <SwiperSlide key={idx} className="relative w-full h-full">
+            <Image
+              src={src}
+              alt={`Slide ${idx + 1}`}
+              fill
+              priority
+              sizes="100vw"
+              className="object-cover object-center"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
